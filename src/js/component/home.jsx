4870127de4870dddebfb981455cd8f4d0fe654ae
Zurda13 <144481470+Zurda13@users.noreply.github.com>
2023-12-03 17:33:56 +0000

    creacion lista de tareas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function TodoList() {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState('');
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+    const addTask = () => {
+        if (newTask !== '') {
+            setTasks([...tasks, newTask]);
+            setNewTask('');
+        }
+    };
 
-export default Home;
+    const removeTask = (index) => {
+        setTasks(tasks.filter((_, i) => i !== index));
+    };
+
+    const updateCounter = () => {
+        return tasks.length;
+    };
+
+    return (
+        <div className="container" style={{maxWidth: '400px', margin: '0 auto', padding: '1rem', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', borderRadius: '8px', backgroundColor: '#ffffff'}}>
+            <h1 className="text-center mt-5">ToDo List</h1>
+            <input type="text" value={newTask} onChange={e => setNewTask(e.target.value)} placeholder="Enter new task..."/>
+            <button onClick={addTask} className="btn btn-primary">Add Task</button>
+            <ul className="list-group mt-3">
+                {tasks.map((task, index) => (
+                    <li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+                        {task}
+                        <button onClick={() => removeTask(index)} className="btn btn-danger">x</button>
+                    </li>
+                ))}
+            </ul>
+            <p className="text-center mt-3">Total tasks: {updateCounter()}</p>
+        </div>
+    );
+}
+
+export default TodoList;
